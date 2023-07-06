5c2c17c0dee68f1990ccefad5828150bfcbb807d
Sazzadur Rahman <ssasahil000@gmail.com>
2023-07-06 07:10:29 +0530

    added-loading-pages

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,6 +5,7 @@ import { signIn } from "next-auth/react";
 
 import Button from "@/components/ui/Button";
 import { toast } from "react-hot-toast";
+import { Icons } from "@/components/Icons";
 
 interface pageProps {}
 
@@ -27,7 +28,7 @@ const LoginPage: FC<pageProps> = ({}) => {
 		<div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
 			<div className="w-full flex flex-col items-center max-w-md space-y-8">
 				<div className="flex flex-col items-center gap-8">
-					logo
+					<Icons.Logo className="h-14 w-auto text-indigo-600" />
 					<h2 className="mt6 text-center text-3xl font-bold tracking-tight text-gray-900">
 						Sign in to your account
 					</h2>
